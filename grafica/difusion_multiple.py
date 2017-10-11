9643e58a6bddd0598d479f718b2042ebe213f63a
Nacho Castaneda <icastanedaw@gmail.com>
2017-10-11 15:48:07 -0300

    Difusion nueva doble linea

--- grafica/difusion_multiple.py
+++ grafica/difusion_multiple.py
@@ -9,6 +9,7 @@ def mayus(string):
     return " ".join([s[0].upper() + s[1:] for s in string.split()])
 
 def crear_difusion_multiple(vendedor, productos):
+    productos.sort(key=lambda k: k["orden"])
     n = len(productos)
     productos1, productos2 = productos[:n//2], productos[n//2:]
     ancho_franja = 2000//n
@@ -18,7 +19,7 @@ def crear_difusion_multiple(vendedor, productos):
     ancho_producto = int(ancho_franja*0.9)
     y_formato = 400
     y_precio = y_formato + 150
-    y_producto = 600
+    y_producto = 500
     y_nombre = 1700 - 800
     y_telefono = 1785 - 800
     y_circulo = 20
@@ -59,33 +60,39 @@ def crear_difusion_multiple(vendedor, productos):
                     font_numeros.getsize(str_precio)[0])
             x = x_circulo+(circulo_size-w)//2
             y = y_circulo+(circulo_size-h)//2
+            for xo, yo in ((1,0),(0,1),(-1,0),(0,-1)):
+                draw.multiline_text((x_offset+x+xo, y+yo),
+                                    texto, (0, 0, 0),
+                                    font_numeros, align="center")
             draw.multiline_text((x_offset+x, y),
                                 texto, (255, 255, 255),
                                 font_numeros, align="center")
             
             nombre = producto["producto"]
             detalle = None
-            if len(nombre.split()) > 1:
-                nombre, detalle = nombre.split()
-            lineas_producto = cantidad_lineas
-            while len(nombre)/lineas_producto <= 2:
-                lineas_producto -= 1
-            largo_linea = math.ceil(len(nombre)/lineas_producto)
-            i = 0
-            lineas = []
-            while len(lineas) < lineas_producto:
-                if len(lineas)+2 == lineas_producto and len(nombre)%largo_linea == 1:
-                        j = min(i+largo_linea+1, len(nombre))
-                        lineas.append(nombre[i:j])
-                        lineas_producto -= 1
-                        i += 1
-                else:
-                    j = min(i+largo_linea, len(nombre))
-                    lineas.append(nombre[i:j])
-                i += largo_linea
-            if detalle is not None:
-                lineas.append(detalle)
-                lineas_producto += 1
+##            if len(nombre.split()) > 1:
+##                nombre, detalle = nombre.split()
+##            lineas_producto = cantidad_lineas
+##            while len(nombre)/lineas_producto <= 2:
+##                lineas_producto -= 1
+##            largo_linea = math.ceil(len(nombre)/lineas_producto)
+##            i = 0
+##            lineas = []
+##            while len(lineas) < lineas_producto:
+##                if len(lineas)+2 == lineas_producto and len(nombre)%largo_linea == 1:
+##                        j = min(i+largo_linea+1, len(nombre))
+##                        lineas.append(nombre[i:j])
+##                        lineas_producto -= 1
+##                        i += 1
+##                else:
+##                    j = min(i+largo_linea, len(nombre))
+##                    lineas.append(nombre[i:j])
+##                i += largo_linea
+##            if detalle is not None:
+##                lineas.append(detalle)
+##                lineas_producto += 1
+            lineas = nombre.split()
+            lineas_producto = len(lineas)
             y_offset = y_producto
             for i, linea in enumerate(lineas):
                 s = 180
@@ -105,18 +112,18 @@ def crear_difusion_multiple(vendedor, productos):
                 w, h = font_producto.getsize(linea)
                 h *= 0.75
                 x, y = x_offset+(ancho_franja-w)//2, y_offset-h/2
-##                draw.text((x+1, y), linea, (0, 0, 0, 0), font_producto)
-##                draw.text((x, y+1), linea, (0, 0, 0, 0), font_producto)
-##                draw.text((x-1, y), linea, (0, 0, 0, 0), font_producto)
-##                draw.text((x, y-1), linea, (0, 0, 0, 0), font_producto)
+                draw.text((x+1, y), linea, (0, 0, 0, 0), font_producto)
+                draw.text((x, y+1), linea, (0, 0, 0, 0), font_producto)
+                draw.text((x-1, y), linea, (0, 0, 0, 0), font_producto)
+                draw.text((x, y-1), linea, (0, 0, 0, 0), font_producto)
                 draw.text((x, y), linea, (255, 255, 255), font_producto)
                 y_offset += h
             x_offset += ancho_franja
         y_offset_prod += 1200
-        y_formato += 1200
+        y_formato = 2000 - circulo_size
         y_precio = y_formato + 150
-        y_producto += 1200
-        y_circulo += 1200
+        y_producto += 1000
+        y_circulo = 2000 - circulo_size - 20
     img.paste(Image.open("grafica/Fotos/banner.jpg"), (0, alto-400-800))
     nombre = vendedor["nombre"]
     apellido = vendedor["apellido"]
