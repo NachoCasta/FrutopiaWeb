664b10bf32859db47cbf78bf3ba205ca5fef492b
Nacho Castaneda <icastanedaw@gmail.com>
2017-11-08 14:49:24 -0300

    Fix cobranza

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def telegram_webhook():
             bot.sendMessage(chat_id, str(err))
         if len(list(respuesta)) == 2:
             respuesta, status = respuesta
-        if stats == "error":
+        if status == "error":
             bot.sendMessage(chat_id, respuesta)
         if status == "wait":
             bot.sendMessage(chat_id, respuesta, "Markdown")
